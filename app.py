ef84011ca834687917e74ea4460681ee3444a242
gabbymorgan <gabriellarosemorgan@gmail.com>
2025-07-01 20:49:56 -0500

    fix request_render, return errors

--- app.py
+++ app.py
@@ -12,22 +12,22 @@ async def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/detect_screen_interaction")
+@app.get("/screen_interaction")
 async def detect_screen_interaction():
     try:
         screen_data = interface.detect_screen_interaction()
         return {"success": True, "screen_data": screen_data}
     except Exception as e:
-        return {"success": False}
+        return {"success": False, "error": str(e)}
 
 
 @app.post("/shutdown")
 async def shutdown():
     try:
-        interface.sleep()
+        interface.shutdown()
         return {"success": True}
     except Exception as e:
-        return {"success": False}
+        return {"success": False, "error": str(e)}
 
 
 @app.post("/sleep")
@@ -36,7 +36,7 @@ async def sleep():
         interface.sleep()
         return {"success": True}
     except Exception as e:
-        return {"success": False}
+        return {"success": False, "error": str(e)}
 
 
 @app.post("/awaken")
@@ -45,7 +45,7 @@ async def awaken():
         interface.awaken()
         return {"success": True}
     except Exception as e:
-        return {"success": False}
+        return {"success": False, "error": str(e)}
 
 
 @app.post("/clear_screen")
@@ -54,7 +54,7 @@ async def clear_screen():
         interface.clear_screen()
         return {"success": True}
     except Exception as e:
-        return {"success": False}
+        return {"success": False, "error": str(e)}
 
 
 @app.post("/reset_canvas")
@@ -63,35 +63,25 @@ async def reset_canvas():
         interface.reset_canvas()
         return {"success": True}
     except Exception as e:
-        return {"success": False}
+        return {"success": False, "error": str(e)}
 
 
-@app.post("/render")
-async def render(file: Annotated[bytes, File()]):
+@app.post("/request_render")
+async def request_redner(file: Annotated[bytes, File()]):
     try:
-        print(len(file))
         interface.request_render(image_data=file)
         return {"success": True}
     except Exception as e:
         return {"success": False, "error": str(e)}
 
 
-@app.post("/request_render")
-async def request_render():
-    try:
-        interface.request_render()
-        return {"success": True}
-    except Exception as e:
-        return {"success": False}
-
-
-@app.post("/get_alignment")
+@app.get("/window")
 async def get_alignment():
     try:
-        alignment_data = interface.get_alignment()
+        alignment_data = interface.get_window()
         return {
             "success": True,
             "alignment_data": alignment_data
         }
     except Exception as e:
-        return {"success": False}
+        return {"success": False, "error": str(e)}
